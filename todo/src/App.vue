0c04106d5af3775bcd983f092b73eb8b78972897
Brooks Poltl <bpoltl1@gmail.com>
2020-09-24 17:37:46 -0500

    todo

---
+++ todo/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <input v-model="text" class="border" type="text" />
+    <button @click="addTodo(text)">add</button>
+    <div>
+      <div
+        v-for="(todo, index) in todos"
+        :key="index"
+        class="w-4/5 flex justify-between"
+      >
+        <p>{{ todo }}</p>
+        <button @click="deleteTodo(index)">delete</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const todos: string[] = reactive([]);
+    const text = ref('');
+    const addTodo = (todo: string) => {
+      todos.push(todo);
+    };
+    const deleteTodo = (index: number) => {
+      todos.splice(index, 1);
+    };
+    return { todos, text, deleteTodo, addTodo };
+  },
+});
+</script>
